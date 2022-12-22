34e25d750d24e0134fef2502a72dc14db2039564
Hamzaansari <hamzaansaree15@gmail.com>
2022-12-22 11:43:44 +0500

    Initialize React-Native App

---
+++ AwesomeProject/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Login } from './assets/login';
+
+const App = () => {
+ 
+
+  return (
+    <Login />
+    // <View>
+    //   <Text>Hamza</Text>
+    // </View>
+  );
+};
+
+
+
+export default App;
