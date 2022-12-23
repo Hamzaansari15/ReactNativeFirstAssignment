406b48cb8f5bf635557742d020a9b9ffb4921026
Hamzaansari <hamzaansaree15@gmail.com>
2022-12-24 01:42:09 +0500

    Complete Login Signup App

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Login } from './assets/login';
+import { Signup } from './assets/signup';
 
 const App = () => {
 
+  const [showLogin, setShowLogin] = useState(true);
+  const [showSignup, setShowSignup] = useState(false)
+
+  const signup = () => {
+    setShowLogin(false);
+    setShowSignup(true);
+  }
+
+  const login = () => {
+    setShowLogin(true);
+    setShowSignup(false);
+  }
 
   return (
     <>
-      <SafeAreaView style={styles.main}>
-        <Login />
-      </SafeAreaView>
+      <View style={styles.main}>
+        {showLogin ? <Login signup={signup}/> : null}
+        {showSignup ? <Signup login={login} /> : null}
+      </View>
     </>
   );
 };
@@ -20,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#035efc'
+    backgroundColor: '#4070f4'
   }
 })
 
